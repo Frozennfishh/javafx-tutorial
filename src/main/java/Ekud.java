f511e0d182d34842fbe4c49a56e40325f434c1e4
Frozennfishh <leonard.ong0108@gmail.com>
2025-02-08 00:42:30 +0800

    Add Images, window structure, echo functionality

---
+++ src/main/java/Ekud.java
@@ -0,0 +1,12 @@
+public class Ekud {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Ekud heard: " + input;
+    }
+}
