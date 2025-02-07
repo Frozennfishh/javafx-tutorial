08f09a0d4be94f0d57b47d22dd507bbf3f6788b2
Frozennfishh <leonard.ong0108@gmail.com>
2025-02-08 00:16:22 +0800

    Create Launcher and EkudApp file

---
+++ src/main/java/EkudApp.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class EkudApp extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our scene
+        stage.show(); // Render the stage.
+    }
+}
